f622486afbe47e225f7767470de0e90362d1b35c
s1kko1337 <r.melo.kk@gmail.com>
2025-06-20 15:13:27 +0300

    Add composer to project, implemented 3 generative design patterns.

---
+++ app/patterns/builder/builder.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace Builder;
+
+interface SQLQueryBuilder
+{
+    public function select(string $table, array $fields): SQLQueryBuilder;
+
+    public function where(string $field, string $value, string $operator = '='): SQLQueryBuilder;
+
+    public function limit(int $start,  int $offset): SQLQueryBuilder;
+
+    //...
+
+    public function getSQL(): string;
+}
+
+class MysqlQueryBuilder implements SQLQueryBuilder
+{
+    protected  $query;
+
+    protected function reset(): void
+    {
+        $this->query = new \stdClass;
+    }
+
+    public function select(string $table, array $fields): SQLQueryBuilder
+    {
+        $this->reset();
+        $this->query->base = "SELECT " . implode(' ', $fields) . "FROM " . $table;
+        $this->query->type = 'select';
+
+        return $this;
+    }
+
+    public function where(string $field, string $value, string $operator = '='): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select', 'update', 'delete'])) 
+        {
+            throw new \Exception("WHERE can only be added to SELECT, UPDATE OR DELETE");
+        }
+        $this->query->where[] = "$field $operator '$value'";
+
+        return $this;
+    }
+
+    public function limit(int $start,  int $offset): SQLQueryBuilder
+    {
+        if (!in_array($this->query->type, ['select'])) 
+        {
+            throw new \Exception("LIMIT can only be added to SELECT");
+        }
+        $this->query->limit = " LIMIT " . $start . ", " . $offset;
+
+        return $this;
+    }
+
+    public function getSQL(): string
+    {
+        $query = $this->query;
+        $sql = $query->base;
+        if (!empty($query->where)) {
+            $sql .= " WHERE " . implode(' AND ', $query->where);
+        }
+        if (isset($query->limit)) {
+            $sql .= $query->limit;
+        }
+        $sql .= ";";
+        return $sql;
+    }
+}
+
+//we can make another concrete builder mb for PostgreSQL
+//class PostgresQueryBuilder extends MysqlQueryBuilder{}
+function clientCode(SQLQueryBuilder $queryBuilder)
+{
+    // ...
+
+    $query = $queryBuilder
+        ->select("users", ["name", "email", "password"])
+        ->where("age", 18, ">")
+        ->where("age", 30, "<")
+        ->limit(10, 20)
+        ->getSQL();
+
+    echo $query;
+
+    // ...
+}
+echo "<pre>";
+
+echo "Testing MySQL query builder:\n";
+clientCode(new MysqlQueryBuilder());
+echo "\n\n";
+echo "</pre>";
